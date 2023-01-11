a115a4d2aad1b587f7e175fbeb6afe9a5a2ff0fd
mehebbetbabayeva <mhbbtbabaeva97@gmail.com>
2023-01-11 22:04:41 +0400

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+let a=5;
+let b=6;
+a=a+b; // 11
+b=a-b;  //5
+a=a-b;  //6
+console.log(a,b)
+//console.loga a,b yazdiqda 5,6 evezine 6,5 verir
+
+let c= "salam";
+let d=c.split("").reverse().join("").toString();
+console.log(  d)
+
+// salam stringinin herflerini tersine deyismek
+
+
+
+
+
+
+
